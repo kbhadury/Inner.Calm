8d58437ef355dd711c45e3a03a649f2b041210b0
Kiran Bhadury <kbhadury@ucdavis.edu>
2017-01-21 19:28:25 -0800

    Created js file

---
+++ health.js
@@ -0,0 +1,63 @@
+var main = function(){
+	beenClicked = false;
+	count = 0;
+	enableNotifications = false;
+	
+	$("#mybutton").click(function(){
+		alert("Success!");
+	});
+	
+	$("#addbutton").click(function(){
+		if(!beenClicked){
+			beenClicked = true;
+			setInterval(addItem, 1000*60);
+		}
+	});
+	
+	$("#notifybutton").click(function(){
+		enableNotifications = !enableNotifications; //Toggle notifications
+		
+		//Disable notifications
+		if(!enableNotifications){
+			//clearInterval(intervalFunc);
+			$("#notifybutton").text("Notify me");
+		
+		//Enable notifications
+		} else {
+			//Check if notifications are supported
+			if(!Notification){
+				//Not supported, disable button
+				alert("Notifications are not supported in your browser");
+				enableNotifications = false;
+				$("#notifybutton").text("Notifications not supported").prop("disabled",true);
+				return;
+			} else if(Notification.permission !== "granted"){
+				Notification.requestPermission(function(permission){
+					var n = new Notification("yo!");
+					if(permission === "granted"){
+						$("#notifybutton").text("Stop notifying me!");
+						var n = new Notification("Hi!");
+						//intervalFunc = setInterval(doNotify, 1000*5);
+					} else {
+						//Not permitted, disable button
+						enableNotifications = false;
+						$("#notifybutton").text("Notifications not permitted").prop("disabled",true);
+						return;
+					}
+				});
+			}		
+		}
+	});
+};
+
+function addItem(){
+	$("#mydiv").append($("<p>This is number " + count + "</p>"));
+	count = count + 1;
+};
+
+function doNotify(){
+	var notification = new Notification("Here's the title!",{body: "The count is " + count});
+	count = count + 1;
+}
+
+$(document).ready(main);
